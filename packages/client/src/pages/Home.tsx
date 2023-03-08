1e66fcd14dbbff772be6eab894ff829d3fe2fe9b
Anton Smirnov <tosha.smirnov.2002@gmail.com>
2023-03-08 18:47:14 +0300

    added MiniCssExtractPlugin; support for css-moduleы

--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import './Home.scss';
+import styles from './Home.module.scss';
 
 export function Home() {
     return (
-        <div className="home-page">
+        <div className={styles.home}>
             <h1>Home Page</h1>
         </div>
     );
--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,44 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-console.log(process.env);
-
-module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: '/node_modules/',
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'build'),
+module.exports = (_, options) => {
+    return {
+        entry: './src/index.tsx',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'build'),
         },
-        port: 3000,
-        hot: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html'),
-        }),
-    ],
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        {
+                            loader:
+                                options.mode !== 'production'
+                                    ? 'style-loader'
+                                    : MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: '/node_modules/',
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.(ts|tsx)$/,
+                    loader: 'ts-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'build'),
+            },
+            port: 3000,
+            hot: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'public', 'index.html'),
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+            }),
+        ],
+    };
 };
